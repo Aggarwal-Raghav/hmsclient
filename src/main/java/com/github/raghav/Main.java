cad29a9888336ced2d1f4c34c7e7f2939f58efc0
Raghav Aggarwal <raghavaggarwal03.ra@gmail.com>
2024-09-12 22:31:34 +0530

    init commit

---
+++ src/main/java/com/github/raghav/Main.java
@@ -0,0 +1,11 @@
+package com.github.raghav;
+
+import java.net.URI;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+//    URI hms = getServerUri(null, null)
+//    HMSClient client = new HMSClient( ,"/Users/raghav/Desktop/setup/hive/conf");
+}
