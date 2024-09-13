2bcdbed985ea2a42df98320dcec68d259030c762
Raghav Aggarwal <raghavaggarwal03.ra@gmail.com>
2024-09-13 09:07:04 +0530

    working

--- src/main/java/com/github/raghav/Main.java
+++ src/main/java/com/github/raghav/Main.java
@@ -1,11 +1,20 @@
 package com.github.raghav;
 
+import org.apache.hadoop.hive.metastore.api.Table;
+import org.apache.thrift.TException;
+
+import javax.security.auth.login.LoginException;
+import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
+
+import static com.github.raghav.Util.getServerUri;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws URISyntaxException, TException, LoginException, IOException, InterruptedException {
+        URI local_uri = getServerUri("127.0.0.1", "9083");
+        HMSClient client = new HMSClient(local_uri ,"/Users/raghav/Desktop/setup/hive/conf");
+        Table tbl = client.getTable("raaggarw","x");
+        System.out.println(tbl);
     }
-//    URI hms = getServerUri(null, null)
-//    HMSClient client = new HMSClient( ,"/Users/raghav/Desktop/setup/hive/conf");
 }
